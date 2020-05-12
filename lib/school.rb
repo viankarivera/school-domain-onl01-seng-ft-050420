b47bb27d456169f0d65d6c1d777783e1054d6bb6
Vianka Rivera <viankalrivera@gmail.com>
2020-05-12 13:10:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,11 +4,8 @@ class School
     @name = name
     @roster = {}
   end
-  def school=(school)
-    @school = school
-  end
-  def school
-    @school
-  end
+  def roster
+    @roster = roster
+  end 
 
 end
