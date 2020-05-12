8750e576ae55bd4f80b38d1fb5d8962bf5d0559c
Vianka Rivera <viankalrivera@gmail.com>
2020-05-12 13:49:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,3 +7,15 @@ class School
   def roster
     @roster
   end
+  def add_student(name, grade)
+   @student_name = name
+   @grade = grade
+   if @roster.include?(grade) == false
+     @roster[grade] = []
+   end
+   @roster[grade] << name
+ end
+
+ def grade(number)
+   @roster[number]
+ end
